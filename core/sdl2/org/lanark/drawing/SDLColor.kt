a5d6f1e1893206cf88ff75cc9b21f7ad903a3dc4
Ilya Ryzhenkov <orangy@jetbrains.com>
2018-09-24 00:13:14 +0300

    Cleanup build files

--- core/sdl2/org/lanark/drawing/SDLColor.kt
+++ core/sdl2/org/lanark/drawing/SDLColor.kt
@@ -3,6 +3,6 @@ package org.lanark.drawing
 import kotlinx.cinterop.*
 import sdl2.*
 
-fun Color.toRawColor(format: CPointer<SDL_PixelFormat>?): Uint32 {
+fun Color.toRawColor(format: CPointer<SDL_PixelFormat>?): UInt {
     return SDL_MapRGBA(format, red, green, blue, alpha)
 }
