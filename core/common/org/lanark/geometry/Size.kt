487b49f6d8c942aa50c402a53945b598f271a1db
Ilya Ryzhenkov <orangy@jetbrains.com>
2018-10-02 03:02:05 +0300

    Add Size div Double operation for convenience

--- core/common/org/lanark/geometry/Size.kt
+++ core/common/org/lanark/geometry/Size.kt
@@ -4,6 +4,7 @@ data class Size(val width: Int, val height: Int) {
     operator fun plus(other: Size) = Size(width + other.width, height + other.height)
     operator fun minus(other: Size) = Size(width - other.width, height - other.height)
     operator fun div(other: Size) = Scale(width.toDouble() / other.width, height.toDouble() / other.height)
+    operator fun div(scale: Double) = Size((width / scale).toInt(), (height / scale).toInt())
     operator fun times(scale: Scale) = Size((width * scale.horizontal).toInt(), (height * scale.vertical).toInt())
     override fun toString() = "{$width x $height}"
     operator fun times(scale: Double) = Size((width * scale).toInt(), (height * scale).toInt())
