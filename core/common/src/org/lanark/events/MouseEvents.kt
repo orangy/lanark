6580ea9fcc49ad754195f560c59aed9d92b4d4dd
Ilya Ryzhenkov <orangy@jetbrains.com>
2018-11-25 22:54:33 +0300

    Extract MousePosition events for events where mouse coordinates are known

--- core/common/src/org/lanark/events/MouseEvents.kt
+++ core/common/src/org/lanark/events/MouseEvents.kt
@@ -3,22 +3,25 @@ package org.lanark.events
 import org.lanark.application.*
 import org.lanark.geometry.*
 
-abstract class EventMouse(timestamp: ULong, val frame: Frame) : Event(timestamp) {
+abstract class EventMouse(timestamp: ULong, val frame: Frame) : Event(timestamp)
 
-}
+abstract class EventMousePosition(
+    timestamp: ULong,
+    frame: Frame,
+    val x: Int,
+    val y: Int
+) : EventMouse(timestamp, frame)
+
+val EventMousePosition.position: Point get() = Point(x, y)
 
 abstract class EventMouseButton(
     timestamp: ULong,
     frame: Frame,
     val button: MouseButton,
     val clicks: UInt,
-    val x: Int,
-    val y: Int
-) : EventMouse(timestamp, frame) {
-
-}
-
-val EventMouseButton.position: Point get() = Point(x, y)
+    x: Int,
+    y: Int
+) : EventMousePosition(timestamp, frame, x, y)
 
 class EventMouseButtonDown(timestamp: ULong, frame: Frame, button: MouseButton, x: Int, y: Int, clicks: UInt) :
     EventMouseButton(timestamp, frame, button, clicks, x, y)
@@ -26,22 +29,17 @@ class EventMouseButtonDown(timestamp: ULong, frame: Frame, button: MouseButton, 
 class EventMouseButtonUp(timestamp: ULong, frame: Frame, button: MouseButton, x: Int, y: Int, clicks: UInt) :
     EventMouseButton(timestamp, frame, button, clicks, x, y)
 
-
 class EventMouseMotion(
     timestamp: ULong,
     frame: Frame,
-    val x: Int,
-    val y: Int,
+    x: Int,
+    y: Int,
     val deltaX: Int,
     val deltaY: Int
-) : EventMouse(timestamp, frame) {
-}
-
-val EventMouseMotion.position: Point get() = Point(x, y)
+) : EventMousePosition(timestamp, frame, x, y)
 
 class EventMouseScroll(timestamp: ULong, frame: Frame, val scrollX: Int, val scrollY: Int) :
-    EventMouse(timestamp, frame) {
-}
+    EventMouse(timestamp, frame)
 
 enum class MouseButton {
     Left,
